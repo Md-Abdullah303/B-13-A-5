555ed6567986e499b58d20a04cbea77db3097fe1
Mohammad Abdullah <mdabdulla01715940008@gmail.com>
2026-03-08 01:13:20 +0600

    home page ar navigation is done acording figma

---
+++ js/script.js
@@ -0,0 +1,14 @@
+
+
+const logingBtn = ()=>{
+    const usernamevalue = document.getElementById("username").value;
+    const passwordValue = document.getElementById("password").value;
+    
+    if(usernamevalue == "admin" && passwordValue == "admin123"){
+        alert("LogIn successful");
+        // window.location.assign("./home.html") ata kaj kore nai
+        // window.location.href = "home.html";
+    }else{
+        alert("LogIn error.")
+    }
+}
