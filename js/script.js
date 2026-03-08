24ae44fc865a91ac2278013bb9dc2fbd5ca346c2
Mohammad Abdullah <mdabdulla01715940008@gmail.com>
2026-03-08 11:21:08 +0600

    add for js in script.js also add some more disain home.html

--- js/script.js
+++ js/script.js
@@ -3,6 +3,15 @@
 const logingBtn = ()=>{
     const usernamevalue = document.getElementById("username").value;
     const passwordValue = document.getElementById("password").value;
+
+    if(usernamevalue !== "admin"){
+        alert("Invalid Username.");
+        return;
+    }else if(passwordValue !== "admin123"){
+        alert("Invalid Password.");
+        return;
+    }
+
     
     if(usernamevalue == "admin" && passwordValue == "admin123"){
         alert("LogIn successful");
